f1724cc35f68d2f7cafb3de184b0a803446cae58
Stephen Pidgeon <pidgeon13@googlemail.com>
2021-04-28 17:59:09 +0100

    Simple circle
    Co-authored-by: lakeofdance <lakeofdance@users.noreply.github.com>

--- main.lua
+++ main.lua
@@ -1,4 +1,30 @@
 --Hello Sam
 --Hi
 
---This is a test
+-- window attributes
+window_attr = {resizable = false}
+x_size = 720
+y_size = 720
+
+-- player position
+x_pos = 0
+y_pos = 0
+
+function love.load()
+    -- one-time initial set-up
+    love.window.setMode(x_size, y_size, window_attr)
+    love.window.setTitle('SphereSnake')
+end
+
+function love.update()
+    -- updates the state of the game every frame
+    -- called before love.draw()
+end
+
+function love.draw()
+    -- redraws the game every frame
+    -- called after love.update()
+    love.graphics.circle("line", x_size/2,y_size/2, y_size/3)
+    love.graphics.arc("line", x_size/2,y_size/2 - 10, 100, 0.5, 3)
+    love.graphics.arc( "line", 100, 100, 100, 0, 2)
+end
